37ac58f60de455cf4bdfff6c29577228e92c3bc7
William Rocha <william.bueno.rocha@gmail.com>
2022-01-24 23:34:16 -0300

    global and modules styles

---
+++ src/pages/stylish.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Stylish.module.css'
+
+export default function Stylish(){
+    return(
+        <div className={styles.roxo}>
+            <h1>Estilo usando CSS modulos</h1>
+        </div>
+    )
+}
